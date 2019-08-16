181ab0e409068cc270e1fe261064feac28b2bc96
linds <lindshabani97@hotmail.com>
2019-08-16 10:35:18 +0200

    Finishing templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
 ]
